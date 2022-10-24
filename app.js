c45d65b7a988e3bbb1b25e029738ee152504eada
NishatFariza <nishatfarzia78@gmail.com>
2022-10-24 06:22:10 +0600

    refector: folder structure

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ window.addEventListener("keydown", function (e) {
     console.log(e.keyCode)
 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    if (!audio) return; //stop the function running all togather
-        audio.play();
+    if (!audio) return; //stop the function running all together
+
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
 })
