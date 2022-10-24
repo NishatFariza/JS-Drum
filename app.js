17beee59a12d00546adb19eac5181faf4a485496
NishatFariza <nishatfarzia78@gmail.com>
2022-10-24 08:57:02 +0600

    wip:click event

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
-window.addEventListener("keydown", function (e) {
-    console.log(e.keyCode)
+// Play drum by keyboard press
+window.addEventListener("keydown", playSound);
 
+function playSound(e) {
+    // console.log(e.keyCode)
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return; //stop the function running all together
 
     audio.currentTime = 0; // rewind to the start
     audio.play();
-})
+    
+    
+    if (!key) return;
+    key.classList.add('playing')
+
+}
+
+
+// Remove .playing class after transition action over
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    // console.log(this);
+    this.classList.remove('playing')
+}
+
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener("transitionend", removeTransition))
+
+
+// Play music by mouse click
+keys.forEach(key => key.addEventListener("click", playByMouse))
+function playByMouse(e) {
+    const keyCode = this.getAttribute("data-key");
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    key.classList.add('playing')
+
+
+}
+
+
